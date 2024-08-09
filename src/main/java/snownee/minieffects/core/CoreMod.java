4c76fff0fb2cadd32f283c93134f28733bae97f1
ZZZank <3410764033@qq.com>
2024-08-09 23:14:23 +0800

    mark MiniEffects as coremod again to actually inject mixin

---
+++ src/main/java/snownee/minieffects/core/CoreMod.java
@@ -0,0 +1,44 @@
+package snownee.minieffects.core;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Nullable;
+
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import snownee.minieffects.MiniEffectsInfo;
+import zone.rong.mixinbooter.IEarlyMixinLoader;
+
+public class CoreMod implements IEarlyMixinLoader, IFMLLoadingPlugin {
+
+	@Override
+	public String[] getASMTransformerClass() {
+		return new String[0];
+	}
+
+	@Override
+	public String getModContainerClass() {
+		return null;
+	}
+
+	@Nullable
+	@Override
+	public String getSetupClass() {
+		return null;
+	}
+
+	@Override
+	public void injectData(Map<String, Object> data) {
+	}
+
+	@Override
+	public String getAccessTransformerClass() {
+		return null;
+	}
+
+	@Override
+	public List<String> getMixinConfigs() {
+		return Collections.singletonList(MiniEffectsInfo.MIXIN_CONFIG);
+	}
+}
