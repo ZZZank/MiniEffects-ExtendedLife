bc2b9bf3f12d57e05676b1ba804298aef19fa892
ZZZank <3410764033@qq.com>
2024-08-27 21:11:33 +0800

    rewrite mixin, split area into two areas(icon & expanded) for better mouse over detection

--- src/main/java/snownee/minieffects/mixin/MixinDisplayEffectsScreen.java
+++ src/main/java/snownee/minieffects/mixin/MixinDisplayEffectsScreen.java
@@ -4,11 +4,10 @@ import java.awt.Rectangle;
 import java.util.Collections;
 import java.util.List;
 
-import net.minecraft.inventory.Container;
+import com.google.common.collect.ImmutableList;
+import net.minecraft.potion.Potion;
 import org.lwjgl.input.Mouse;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.renderer.GlStateManager;
@@ -27,42 +26,47 @@ import snownee.minieffects.MiniEffectsConfig;
 
 @Mixin(InventoryEffectRenderer.class)
 public abstract class MixinDisplayEffectsScreen extends GuiContainer implements IAreasGetter {
-    @Unique
-    private final Rectangle area = new Rectangle();
-    @Unique
-    private boolean expand;
-    @Unique
-    private int effects;
-    @Unique
-    private final ItemStack iconItem = new ItemStack(Items.POTIONITEM);
 
-    private MixinDisplayEffectsScreen(Container inventorySlotsIn) {
-        super(inventorySlotsIn);
+    @Unique
+    private final Rectangle miniEff$iconArea = new Rectangle();
+    @Unique
+    private final Rectangle miniEff$expandedArea = new Rectangle();
+    @Unique
+    private final List<Rectangle> miniEff$cachedAreas = ImmutableList.of(miniEff$iconArea, miniEff$expandedArea);
+    @Unique
+    private boolean miniEff$expand;
+    @Unique
+    private int miniEff$effects;
+    @Unique
+    private final ItemStack miniEff$iconItem = new ItemStack(Items.POTIONITEM);
+
+    private MixinDisplayEffectsScreen() {
+        super(null);
     }
 
     @Inject(method = "initGui", at = @At("TAIL"))
     public void miniEffects$init(CallbackInfo ci) {
-        iconItem.setTagCompound(new NBTTagCompound());
+        miniEff$iconItem.setTagCompound(new NBTTagCompound());
         miniEffects$updateArea();
     }
 
     @Inject(method = "drawActivePotionEffects", cancellable = true, at = @At("HEAD"))
     private void miniEffects$render(CallbackInfo ci) {
-        EntityPlayerSP player = mc.player;
 
         int effects = 0, bad = 0;
-        for (PotionEffect effectInstance : mc.player.getActivePotionEffects()) {
-            if (effectInstance.getPotion().shouldRender(effectInstance)) {
+        for (PotionEffect effect : mc.player.getActivePotionEffects()) {
+            Potion potion = effect.getPotion();
+            if (potion.shouldRender(effect)) {
                 ++effects;
-                if (!effectInstance.getPotion().isBeneficial()) {
+                if (!potion.isBeneficial()) {
                     ++bad;
                 }
             }
         }
 
-        if (this.effects != effects) {
-            this.effects = effects;
-            if (effects == 0 || expand) {
+        if (this.miniEff$effects != effects) {
+            this.miniEff$effects = effects;
+            if (effects == 0 || miniEff$expand) {
                 miniEffects$updateArea();
             }
         }
@@ -72,56 +76,63 @@ public abstract class MixinDisplayEffectsScreen extends GuiContainer implements 
         int scaledHeight = scaledresolution.getScaledHeight();
         int x = Mouse.getX() * scaledWidth / mc.displayWidth;
         int y = scaledHeight - Mouse.getY() * scaledHeight / mc.displayHeight - 1;
-        boolean expand = area.contains(x, y);
-        if (expand != this.expand) {
-            this.expand = expand;
+
+        boolean shouldExpand = miniEff$iconArea.contains(x, y);
+        if (this.miniEff$expand) { //prevent shrinking if in expanded area and currently expanded
+            shouldExpand |= miniEff$expandedArea.contains(x, y);
+        }
+        if (shouldExpand != this.miniEff$expand) {
+            this.miniEff$expand = shouldExpand;
             miniEffects$updateArea();
         }
 
-        if (effects <= 0 || expand) {
+        if (effects <= 0 || shouldExpand) {
             return;
         }
+        miniEffects$renderUnexpanded(effects, bad);
+        ci.cancel();
+    }
+
+    @Unique
+    private void miniEffects$renderUnexpanded(int effectsTotal, int effectsBad) {
         mc.getTextureManager().bindTexture(GuiContainer.INVENTORY_BACKGROUND);
         GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-        x = area.x;
-        y = area.y;
+        int x = miniEff$expand ? miniEff$expandedArea.x : miniEff$iconArea.x;
+        int y = miniEff$expand ? miniEff$expandedArea.y : miniEff$iconArea.y;
         this.drawTexturedModalRect(x, y, 141, 166, 24, 24);
 
-        int color = player.getDataManager().get(AccessLivingEntity.getParameter());
-        iconItem.getTagCompound().setInteger("CustomPotionColor", color);
-        mc.getRenderItem().renderItemAndEffectIntoGUI(iconItem, x + 3, y + 4);
+        int color = mc.player.getDataManager().get(AccessLivingEntity.getParameter());
+        miniEff$iconItem.getTagCompound().setInteger("CustomPotionColor", color);
+        mc.getRenderItem().renderItemAndEffectIntoGUI(miniEff$iconItem, x + 3, y + 4);
         GlStateManager.pushMatrix();
         GlStateManager.translate(0, 0, 200);
         x += 22;
         y += 14;
-        if (effects - bad > 0) {
-            String s = Integer.toString(effects - bad);
+        if (effectsTotal - effectsBad > 0) {
+            String s = Integer.toString(effectsTotal - effectsBad);
             mc.fontRenderer.drawStringWithShadow(s, x - mc.fontRenderer.getStringWidth(s), y, 16777215);
             y -= 10;
         }
-        if (bad > 0) {
-            String s = Integer.toString(bad);
+        if (effectsBad > 0) {
+            String s = Integer.toString(effectsBad);
             mc.fontRenderer.drawStringWithShadow(s, x - mc.fontRenderer.getStringWidth(s), y, 16733525);
         }
         GlStateManager.popMatrix();
-        ci.cancel();
     }
 
     @Unique
     private void miniEffects$updateArea() {
         int left = this.getGuiLeft();
         int top = this.getGuiTop();
-        if (expand) {
-            area.setBounds(left - 124, top, 119, Math.min(33 * 5, 33 * effects));
+        if (miniEff$expand) {
+            miniEff$expandedArea.setBounds(left - 124, top, 119, Math.min(33 * 5, 33 * miniEff$effects));
         } else {
-            area.setBounds(left - 25 + MiniEffectsConfig.xOffset, top + MiniEffectsConfig.yOffset, 20, 20);
+            miniEff$iconArea.setBounds(left - 25 + MiniEffectsConfig.xOffset, top + MiniEffectsConfig.yOffset, 20, 20);
         }
     }
 
     @Override
     public List<Rectangle> getAreas() {
-        return effects == 0
-            ? Collections.emptyList()
-            : Collections.singletonList(area);
+        return miniEff$effects == 0 ? Collections.emptyList() : miniEff$cachedAreas;
     }
 }
