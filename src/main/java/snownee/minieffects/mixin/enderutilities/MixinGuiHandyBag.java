d039b67e7c2692d6235300cd249591e4504dfe0e
ZZZank <3410764033@qq.com>
2024-11-10 00:10:03 +0800

    Ender Utilities noted

---
+++ src/main/java/snownee/minieffects/mixin/enderutilities/MixinGuiHandyBag.java
@@ -0,0 +1,11 @@
+package snownee.minieffects.mixin.enderutilities;
+
+import fi.dy.masa.enderutilities.gui.client.GuiHandyBag;
+import org.spongepowered.asm.mixin.Mixin;
+
+/**
+ * @author ZZZank
+ */
+@Mixin(GuiHandyBag.class)
+public abstract class MixinGuiHandyBag {
+}
