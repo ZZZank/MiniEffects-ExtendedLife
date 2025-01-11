ed25c1c843efc7be5a0df88f4d5f7087b78b4cbd
ZZZank <3410764033@qq.com>
2025-01-11 16:36:14 +0800

    doc for x/y offset config, and per screen offset config

--- src/main/java/snownee/minieffects/MiniEffectsConfig.java
+++ src/main/java/snownee/minieffects/MiniEffectsConfig.java
@@ -7,6 +7,15 @@ import net.minecraftforge.common.config.Config;
 @Config(modid = MiniEffects.ID)
 public class MiniEffectsConfig {
 
-	public int xOffset;
-	public int yOffset;
+	@Config.Comment("default x offset of effect icon rendering position")
+	public int xOffset = 0;
+	@Config.Comment("default y offset of effect icon rendering position")
+	public int yOffset = 0;
+	@Config.Comment({
+		"Screen specific x/y offset of effect icon rendering position",
+		"each entry should be in 'type;x;y' format",
+		"let's say if you want y offset to be 20 when in creative inventory, then 'type' should be the class name of creative menu",
+		"the whole entry will then be 'net.minecraft.client.gui.inventory.GuiContainerCreative;0;20'"
+	})
+	public String[] offsetPerScreen = new String[0];
 }
