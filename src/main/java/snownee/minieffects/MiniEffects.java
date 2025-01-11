c00c0752a042ba404b8c8e7a631a93b55cf55409
ZZZank <3410764033@qq.com>
2025-01-11 15:56:23 +0800

    use annotation for event listener registering

--- src/main/java/snownee/minieffects/MiniEffects.java
+++ src/main/java/snownee/minieffects/MiniEffects.java
@@ -1,9 +1,6 @@
 package snownee.minieffects;
 
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
-import snownee.minieffects.handlers.MiniEffectsEventHandlers;
 
 @Mod(
 	modid = MiniEffectsInfo.MOD_ID,
@@ -16,8 +13,5 @@ public class MiniEffects {
 	public static final String ID = MiniEffectsInfo.MOD_ID;
 
 	public MiniEffects() {
-		if (FMLCommonHandler.instance().getSide().isClient()) {
-			MinecraftForge.EVENT_BUS.register(MiniEffectsEventHandlers.class);
-		}
 	}
 }
--- src/main/java/snownee/minieffects/handlers/MiniEffectsEventHandlers.java
+++ src/main/java/snownee/minieffects/handlers/MiniEffectsEventHandlers.java
@@ -5,23 +5,27 @@ import net.minecraftforge.common.config.Config;
 import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
 import snownee.minieffects.MiniEffects;
+import net.minecraftforge.fml.common.Mod;
+
+import javax.annotation.Nonnull;
 
 /**
  * @author ZZZank
  */
-@Config(modid = MiniEffects.ID)
+@Mod.EventBusSubscriber(value = Side.CLIENT, modid = MiniEffects.ID)
 public class MiniEffectsEventHandlers {
 
     @SubscribeEvent
-    public static void onConfigReload(ConfigChangedEvent.OnConfigChangedEvent event) {
+    public static void onConfigReload(@Nonnull ConfigChangedEvent.OnConfigChangedEvent event) {
         if (event.getModID().equals(MiniEffects.ID)) {
             ConfigManager.sync(MiniEffects.ID, Config.Type.INSTANCE);
         }
     }
 
     @SubscribeEvent
-    public static void onPotionShift(GuiScreenEvent.PotionShiftEvent event) {
+    public static void onPotionShift(@Nonnull GuiScreenEvent.PotionShiftEvent event) {
         event.setCanceled(true);
     }
 }
